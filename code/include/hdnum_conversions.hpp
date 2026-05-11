28cb004cbf8c83a0f54cd75e53ce019b78f26d8b
Igor Dimitrov <igor-dimi@protonmail.com>
2026-05-11 09:54:47 +0200

    convert functions working

--- code/include/hdnum_conversions.hpp
+++ code/include/hdnum_conversions.hpp
@@ -1,11 +1,53 @@
 #pragma once
 
 #include <cstddef>
+#include <stdexcept>
+#include <type_traits>
+
 #include "hdnum.hh"
 
 namespace mpir {
 
-// Vector conversion: resizes output if needed.
+// Helper for dependent static_assert(false)
+template<class...>
+struct always_false : std::false_type {};
+
+
+// Detect HDNUM's GMP wrapper type hdnum::FP<m>
+template<class T>
+struct is_hdnum_fp_impl : std::false_type {};
+
+template<int m>
+struct is_hdnum_fp_impl<hdnum::FP<m>> : std::true_type {};
+
+template<class T>
+inline constexpr bool is_hdnum_fp_v =
+    is_hdnum_fp_impl<
+        std::remove_cv_t<std::remove_reference_t<T>>
+    >::value;
+
+
+// Scalar conversion
+template<class T_out, class T_in>
+T_out scalar_cast(const T_in& x)
+{
+    if constexpr (std::is_constructible_v<T_out, const T_in&>) {
+        return T_out(x);
+    }
+    else if constexpr (
+        is_hdnum_fp_v<T_in> &&
+        std::is_constructible_v<T_out, double>
+    ) {
+        return T_out(x.getNumber().get_d());
+    }
+    else {
+        static_assert(always_false<T_out, T_in>::value,
+                      "mpir::scalar_cast: unsupported scalar conversion");
+    }
+}
+
+
+// Vector conversion
 template<class T_out, class T_in>
 void convert(hdnum::Vector<T_out>& out,
              const hdnum::Vector<T_in>& in)
@@ -15,21 +57,14 @@ void convert(hdnum::Vector<T_out>& out,
     }
 
     for (std::size_t i = 0; i < in.size(); ++i) {
-        out[i] = T_out(in[i]);
+        out[i] = scalar_cast<T_out>(in[i]);
     }
 }
 
-// Convenience wrapper: allocates a new vector.
-template<class T_out, class T_in>
-hdnum::Vector<T_out> convert_vector(const hdnum::Vector<T_in>& in)
-{
-    hdnum::Vector<T_out> out(in.size());
-    convert(out, in);
-    return out;
-}
 
 
-// Matrix conversion: output matrix must already have correct shape.
+
+// DenseMatrix conversion
 template<class T_out, class T_in>
 void convert(hdnum::DenseMatrix<T_out>& out,
              const hdnum::DenseMatrix<T_in>& in)
@@ -42,12 +77,13 @@ void convert(hdnum::DenseMatrix<T_out>& out,
 
     for (std::size_t i = 0; i < in.rowsize(); ++i) {
         for (std::size_t j = 0; j < in.colsize(); ++j) {
-            out[i][j] = T_out(in[i][j]);
+            out[i][j] = scalar_cast<T_out>(in[i][j]);
         }
     }
 }
 
-// Convenience wrapper: allocates a new matrix.
+
+// convenience function
 template<class T_out, class T_in>
 hdnum::DenseMatrix<T_out> convert_matrix(const hdnum::DenseMatrix<T_in>& in)
 {
@@ -56,29 +92,13 @@ hdnum::DenseMatrix<T_out> convert_matrix(const hdnum::DenseMatrix<T_in>& in)
     return out;
 }
 
-
-// template<class T_out, class T_in>
-// void convert(hdnum::DenseMatrix<T_out>& out,
-//              const hdnum::DenseMatrix<T_in>& in)
-// {
-//     if (out.rowsize() != in.rowsize() || out.colsize() != in.colsize()) {
-//         out.resize(in.rowsize(), in.colsize());
-//     }
-
-//     for (std::size_t i = 0; i < in.rowsize(); ++i) {
-//         for (std::size_t j = 0; j < in.colsize(); ++j) {
-//             out[i][j] = T_out(in[i][j]);
-//         }
-//     }
-// }
-
-// template<class T_out, class T_in>
-// hdnum::DenseMatrix<T_out> convert_matrix(const hdnum::DenseMatrix<T_in>& in)
-// {
-//     hdnum::DenseMatrix<T_out> out(in.rowsize(), in.colsize());
-//     convert(out, in);
-//     return out;
-// }
-
+// convenience function
+template<class T_out, class T_in>
+hdnum::Vector<T_out> convert_vector(const hdnum::Vector<T_in>& in)
+{
+    hdnum::Vector<T_out> out(in.size());
+    convert(out, in);
+    return out;
+}
 
 } // namespace mpir
