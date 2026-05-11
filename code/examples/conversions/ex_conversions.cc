b3c9ac28165dee96950244dc1427392b5aba79a3
Igor Dimitrov <igor-dimi@protonmail.com>
2026-05-11 09:53:27 +0200

    conversions examples subfolder

--- code/examples/conversions/ex_conversions.cc
+++ code/examples/conversions/ex_conversions.cc
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <limits>
 
 #include "hdnum.hh"
 #include "hdnum_conversions.hpp"
+
+#include <type_traits>
 
 #define SHOW(x) std::cout << #x << " = " << (x) << '\n'
 
@@ -19,6 +22,7 @@ void print_all(const Ts&... values)
 
 int main()
 {
+    std::cout << std::setprecision(50);
     
     hdnum::FP64 d = 3.14159265359; // double
     // hdnum::FP32 f = 4.0; // float
@@ -98,6 +102,47 @@ int main()
     auto x3 = double(fp_low);
 
     SHOW(x3);
+
+    hdnum::FP256 fp256_number = 1.234567;
+    auto d4 = fp256_number.getNumber().get_d();
+
+
+    SHOW(fp256_number);
+    SHOW(d4);
+
+    mpf_class m("1.123456789012345678901234567890", 128);
+    hdnum::FP128 x001(m);
+
+
+    SHOW(x001);
+    {
+        hdnum::FP16 x = 1.0;
+        hdnum::FP256 z = 1.34;
+        double y = x;
+        auto y2 = double(x);
+        auto y3 = float(x);
+        SHOW(x);
+        SHOW(y);
+        SHOW(y2);
+        SHOW(y3);
+        std::cout << sizeof(y3) << std::endl;
+        auto z3 = static_cast<hdnum::FP256>(x);
+        SHOW(z3);
+        std::cout << "size of z: " << sizeof(z3) << std::endl;
+    }
+
+    std::cout << std::is_constructible_v<double, double> << std::endl;
+    std::cout << std::is_constructible_v<hdnum::FP128, hdnum::FP16> << std::endl;
+    std::cout << "is FP64 constructible from FP16?:  " <<  std::is_constructible_v<hdnum::FP64, hdnum::FP16> << std::endl;
+    std::cout << std::is_constructible_v<hdnum::FP64, hdnum::FP16> << std::endl;
+
+
+    hdnum::FP256 x128 = 1.145;
+    std::cout << "new new " << std::endl;
+    auto y128 = mpir::scalar_cast<hdnum::FP16, hdnum::FP256>(x128);
+
+    SHOW(y128);
+    std::cout << "size of y128: " << sizeof(y128) << std::endl;
 
     return 0;
 }
